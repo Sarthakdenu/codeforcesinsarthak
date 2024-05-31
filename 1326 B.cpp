301d1948b871f64128a5e57724f9fcd035c41db9
Sarthak vyas <sarthakmanju2104@gmail.com>
2024-05-31 15:45:47 +0530

    second commit

--- 1326 B.cpp
+++ 1326 B.cpp
@@ -3,28 +3,13 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
-    int b[n];
-    int a[n];
-    int x[n];
-    for(int i=0;i<n;i++){
-        cin>>b[i];
+     
+    int t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<(n*(n+1))/2<<endl;
     }
-    x[0]=0;
-    a[0]=b[0];
-    a[1]=b[1]+a[0];
-    int temp=0;
-    for(int i=2;i<n;i++){
-        temp=max(a[i-2],temp);
-        a[i]=b[i]+max(a[i-1],temp);
-    }
-   
-  
-    for(int i=0;i<n-1;i++){
-        cout<<a[i]<<" ";
-    }
-    cout<<"hello sarthak"<<endl;
-    cout<<a[n-1]<<endl;
-    
+    return 0;
 }
